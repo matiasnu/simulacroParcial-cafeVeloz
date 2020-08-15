8e0fbf0bc58a7074d89d2ec1e2975491e41ff249
matias.nunez <matias.nunez@intraway.com>
2020-08-14 21:51:48 -0300

    Se cargan los datos

--- cafeVeloz.pl
+++ cafeVeloz.pl
@@ -1,4 +1,30 @@
 :- begin_tests(testFake).
 :- end_tests(testFake).
 
+% jugadores conocidos
+jugador(maradona).
+jugador(chamot).
+jugador(balbo). 
+jugador(caniggia).
+jugador(passarella).
+jugador(pedemonti).
+jugador(basualdo).
+
+% relaciona lo que toma cada jugador
+tomo(maradona, sustancia(efedrina)).
+tomo(maradona, compuesto(cafeVeloz)).
+tomo(caniggia, producto(cocacola, 2)).
+tomo(chamot, compuesto(cafeVeloz)).
+tomo(balbo, producto(gatoreit, 2)). 
+
+% relaciona la máxima cantidad de un producto que 1 jugador puede ingerir
+maximo(cocacola, 3). 
+maximo(gatoreit, 1).
+maximo(naranju, 5).
+
+% relaciona las sustancias que tiene un compuesto
+composicion(cafeVeloz, [efedrina, ajipupa, extasis, whisky, cafe]).
+
+% sustancias prohibidas por la asociación
+sustanciaProhibida(efedrina). sustanciaProhibida(cocaina).
 
